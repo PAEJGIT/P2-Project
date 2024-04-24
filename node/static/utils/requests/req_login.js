6d2dd69b5f9e0f5d676bb2b459cd846d00317414
PAEJGIT <43079636+PAEJGIT@users.noreply.github.com>
2024-04-24 11:22:57 +0200

    Registeringsside tilføjet

--- node/static/utils/requests/req_login.js
+++ node/static/utils/requests/req_login.js
@@ -1,33 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    let loginForm = document.getElementById('loginForm');
-    
-    loginForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+  let loginForm = document.getElementById("loginForm");
 
-        const inputData = {
-            username: loginForm.querySelector('input[name="username"]').value,
-            password: loginForm.querySelector('input[name="password"]').value,
-        };
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        reqLogin(inputData.username, inputData.password);
-    });
+    const inputData = {
+      username: loginForm.querySelector('input[name="username"]').value,
+      password: loginForm.querySelector('input[name="password"]').value,
+    };
 
+    reqLogin(inputData.username, inputData.password);
+  });
+});
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".icon-close");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
 });
 
 function reqLogin(username, password) {
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Log the response from the server
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // Log the response from the server
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
