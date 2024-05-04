aace512700ae5af2975a54ca68f877cec2384315
Stefan <sbrzos19@student.aau.dk>
2024-05-04 16:07:43 +0200

    Udpates

---
+++ node/static/components/footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+	return `
+    <footer>
+        <div id="footer-container-left">
+            <a href="/"><img id="logo" src="../assets/images/logo.png"></a>
+            <span id="footer-container-left-content"> © 2023-current — Aalborg Universitet. All rights reserved.</span>
+        </div>
+        <div id="footer-container-right">
+            <a href="#" class="footer-container-right-links">Privacy Policy</a>
+            <a href="#" class="footer-container-right-links">Terms & Conditions</a>
+            <a href="#" class="footer-container-right-links">Cookie Policy</a>
+            <a href="#" class="footer-container-right-links">Contact</a>
+        </div>
+    </footer>
+    `;
+};
